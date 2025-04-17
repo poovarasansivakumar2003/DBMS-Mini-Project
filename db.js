d20ff3ce3b022a25f7a9df792c817a73678ec015
Poovarasan S <poovarasansivakumar2003@gmail.com>
2025-04-17 18:24:10 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 10,
+    connectionLimit: 5,
     waitForConnections: true,
     queueLimit: 0
 });
